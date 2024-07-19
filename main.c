f0f4a944a24b9fce129ee4ab65d375fd22b690fa
fp91321 <fp91321@github.com>
2024-07-20 00:48:15 +0800

    Add simplified code using function pointers

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*operator)(int,int);
 int main()
 {
 	int a, c;
@@ -12,12 +13,10 @@ int main()
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
 	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	if (b == '+')	operator = plus;
+	else if (b == '-')	operator = minus;
+	else if (b == '*')	operator = multiply;
+	else if (b == '/')	operator = divided;
+
+	printf("%d %c %d = %d\n", a, b, c, operator(a, c));
 }
